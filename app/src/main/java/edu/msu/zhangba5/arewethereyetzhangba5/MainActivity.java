5f4ce71bf0d9636ebebb24602618d23229b0c6df
477project3 <63932785+477project3@users.noreply.github.com>
2020-04-26 15:31:49 -0400

    google map support added

--- app/src/main/java/edu/msu/zhangba5/arewethereyetzhangba5/MainActivity.java
+++ app/src/main/java/edu/msu/zhangba5/arewethereyetzhangba5/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.Address;
@@ -14,6 +15,7 @@ import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.Menu;
@@ -21,6 +23,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -47,8 +50,10 @@ public class MainActivity extends AppCompatActivity {
     private double toLongitude = 0;
     private String to = "";
     private Spinner spinner;
-    private List<String> ModeList;
+    private String[] ModeList;
     private ArrayAdapter<String> adapter;
+    private int TMode = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -64,22 +69,34 @@ public class MainActivity extends AppCompatActivity {
         longitude = -84.487508;
         valid = true;
         // Get the location manager
-        locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
         // Force the screen to say on and bright
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         //Spinner set up
-        spinner = (Spinner)findViewById(R.id.Spinner);
-        ModeList = new ArrayList<String>();
-        ModeList.add("Driving");
-        ModeList.add("Walking");
-        ModeList.add("bicycling");
+        spinner = (Spinner) findViewById(R.id.Spinner);
+        ModeList = getResources().getStringArray(R.array.Mode);
 
-        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,ModeList);
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, ModeList);
 
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int pos, long id) {
+
+                TMode = pos;//1 for Driving, 2 for Walking, 3 for Bicycling
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Another interface callback
+            }
+        });
 
     }
 
@@ -97,13 +114,13 @@ public class MainActivity extends AppCompatActivity {
 
         String bestAvailable = locationManager.getBestProvider(criteria, true);
 
-        if(bestAvailable != null) {
+        if (bestAvailable != null) {
             if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
                     && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                 return;
             }
             locationManager.requestLocationUpdates(bestAvailable, 500, 1, activeListener);
-            TextView viewProvider = (TextView)findViewById(R.id.textProvider);
+            TextView viewProvider = (TextView) findViewById(R.id.textProvider);
             viewProvider.setText(bestAvailable);
             Location location = locationManager.getLastKnownLocation(bestAvailable);
             onLocation(location);
@@ -112,7 +129,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void onLocation(Location location) {
-        if(location == null) {
+        if (location == null) {
             return;
         }
 
@@ -131,17 +148,17 @@ public class MainActivity extends AppCompatActivity {
      * Set all user interface components to the current state
      */
     private void setUI() {
-        TextView temp = (TextView)findViewById(R.id.textTo);
+        TextView temp = (TextView) findViewById(R.id.textTo);
         temp.setText(to);
-        if(!valid){
-            TextView temp1 = (TextView)findViewById(R.id.textLatitude);
+        if (!valid) {
+            TextView temp1 = (TextView) findViewById(R.id.textLatitude);
             temp1.setText(" ");
-            TextView temp2 = (TextView)findViewById(R.id.textDistance);
+            TextView temp2 = (TextView) findViewById(R.id.textDistance);
             temp2.setText(" ");
-            TextView temp3 = (TextView)findViewById(R.id.textLongitude);
+            TextView temp3 = (TextView) findViewById(R.id.textLongitude);
             temp3.setText(" ");
         }
-        if(valid) {
+        if (valid) {
             TextView temp1 = (TextView) findViewById(R.id.textLatitude);
             temp1.setText(String.valueOf(latitude));
             TextView temp2 = (TextView) findViewById(R.id.textLongitude);
@@ -173,7 +190,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
 
-        TextView viewProvider = (TextView)findViewById(R.id.textProvider);
+        TextView viewProvider = (TextView) findViewById(R.id.textProvider);
         viewProvider.setText("");
 
         setUI();
@@ -190,15 +207,15 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void onNew(View view){
+    public void onNew(View view) {
 
-        EditText location = (EditText)findViewById(R.id.editLocation);
+        EditText location = (EditText) findViewById(R.id.editLocation);
         final String address = location.getText().toString().trim();
         newAddress(address);
     }
 
     private void newAddress(final String address) {
-        if(address.equals("")) {
+        if (address.equals("")) {
             // Don't do anything if the address is blank
             return;
         }
@@ -234,7 +251,9 @@ public class MainActivity extends AppCompatActivity {
         public void onProviderDisabled(String s) {
             registerListeners();
         }
-    };
+    }
+
+    ;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -253,7 +272,7 @@ public class MainActivity extends AppCompatActivity {
                 return true;
 
             case R.id.itemHome:
-                newTo("home",42.731138,-84.480541);
+                newTo("home", 42.731138, -84.480541);
                 return true;
 
             case R.id.item2250:
@@ -266,9 +285,10 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * Handle setting a new "to" location.
+     *
      * @param address Address to display
-     * @param lat latitude
-     * @param lon longitude
+     * @param lat     latitude
+     * @param lon     longitude
      */
     private void newTo(String address, double lat, double lon) {
         to = address;
@@ -278,14 +298,15 @@ public class MainActivity extends AppCompatActivity {
         setUI();
 
         SharedPreferences.Editor editor = settings.edit();
-        editor.putString(TOLAT,Double.toString(lat));
-        editor.putString(TOLONG,Double.toString(lon));
-        editor.putString(TO,address);
+        editor.putString(TOLAT, Double.toString(lat));
+        editor.putString(TOLONG, Double.toString(lon));
+        editor.putString(TO, address);
         editor.apply();
     }
 
     /**
      * Look up the provided address. This works in a thread!
+     *
      * @param address Address we are looking up
      */
     private void lookupAddress(final String address) {
@@ -295,7 +316,7 @@ public class MainActivity extends AppCompatActivity {
         List<Address> locations;
         try {
             locations = geocoder.getFromLocationName(address, 1);
-        } catch(IOException ex) {
+        } catch (IOException ex) {
             // Failed due to I/O exception
             locations = null;
             exception = true;
@@ -308,22 +329,22 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void run() {
-                newLocation(address,tempException, tempAddress);
+                newLocation(address, tempException, tempAddress);
             }
         });
     }
 
     private void newLocation(String address, boolean exception, List<Address> locations) {
 
-        if(exception) {
+        if (exception) {
             Toast.makeText(MainActivity.this, R.string.exception, Toast.LENGTH_SHORT).show();
         } else {
-            if(locations == null || locations.size() == 0) {
+            if (locations == null || locations.size() == 0) {
                 Toast.makeText(this, R.string.couldnotfind, Toast.LENGTH_SHORT).show();
                 return;
             }
 
-            EditText location = (EditText)findViewById(R.id.editLocation);
+            EditText location = (EditText) findViewById(R.id.editLocation);
             location.setText("");
 
             // We have a valid new location
@@ -333,7 +354,34 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void onFavorite(View view){
+    public void onFavorite(View view) {
 
     }
+
+    public void onRoute(View view){
+        String Transportation_mode = "";
+        if (TMode == 0){
+            Transportation_mode = "d";
+        }
+        else if (TMode == 1){
+            Transportation_mode = "w";
+        }
+        else if (TMode == 2){
+            Transportation_mode = "b";
+        }
+        settings = PreferenceManager.getDefaultSharedPreferences(this);
+        String lat = settings.getString(TOLAT,"42.731138");
+        String lon = settings.getString(TOLONG,"-84.480507");
+        // Create a Uri from an intent string. Use the result to create an Intent.
+        Uri gmmIntentUri = Uri.parse("google.navigation:q="+lat+","+lon+"&mode="+Transportation_mode);
+
+// Create an Intent from gmmIntentUri. Set the action to ACTION_VIEW
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+// Make the Intent explicit by setting the Google Maps package
+        mapIntent.setPackage("com.google.android.apps.maps");
+
+// Attempt to start an activity that can handle the Intent
+        startActivity(mapIntent);
+    }
 }
+
